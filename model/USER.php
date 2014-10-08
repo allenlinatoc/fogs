b6ef338eb1927340eb4e51a7892827369a03d7bf
Allen Linatoc <linatoc.allen@programmer.net>
2014-10-09 05:01:39 +0800

    Added __Get() method and User info keys

--- model/USER.php
+++ model/USER.php
@@ -9,6 +9,13 @@ final class USER {
     
     const USER_KEY = '__usersess__';
     
+    // info keys
+    const USERNAME = 'username';
+    const PASSWORD = 'password';
+    const TIMESTAMP = 'timestamp';
+    
+    
+    
         /**
      * Encrypts or decrypts sensitive (binary) data
      * @param String $data The data to be processed
@@ -41,16 +48,48 @@ final class USER {
     
     public static function Login($username, $password)
     {
-        $a_userinfos = array();
-        $a_userinfos['username'] = $username;
-        $a_userinfos['password'] = self::Encryptor($password, 'ENCRYPT');
-        $a_userinfos['timestamp'] = time();
-        $_SESSION[self::USER_KEY] = $a_userinfos;
+        $encryptpass = self::Encryptor($password, 'ENCRYPT');
+        $sql = new DB();
+        $sql
+                ->Select()
+                ->From('users')
+                ->Where('`username`="'.$username.'" '
+                        . 'AND `password`="'.$encryptpass.'"');
+        $result = $sql->Query();
+        
+        if (count($result)==0 )
+        {
+            return false;
+        }
+        else
+        {
+            $a_userinfos = array();
+            $a_userinfos['username'] = $username;
+            $a_userinfos['password'] = $encryptpass;
+            $a_userinfos['timestamp'] = time();
+            $_SESSION[self::USER_KEY] = $a_userinfos;
+            return true;
+        }
+    }
+    
+    /**
+     * Gets an information from user's login session
+     * @param string $infokey [null] If null, returns all user infos in array,
+     *      otherwise, returns what was specified (e.g. USER::USERNAME, etc.)
+     * @return mixed
+     */
+    public static function __Get($infokey=null)
+    {
+        if ( $infokey===null )
+        {
+            return $_SESSION[self::USER_KEY];
+        }
+        return $_SESSION[self::USER_KEY][$infokey];
     }
     
     public static function __IsLoggedIn()
     {
-        return key_exists(self::USER_KEY, $_SESSION);
+        return array_key_exists(self::USER_KEY, $_SESSION);
     }
     
 }
