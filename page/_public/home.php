26290a49e6982d112bf9fee2b851b8b8d5da15b8
Allen Linatoc <linatoc.allen@programmer.net>
2014-10-09 05:02:09 +0800

    Home finished

--- page/_public/home.php
+++ page/_public/home.php
@@ -1,16 +1,52 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// Pre-defined values
+$postUsername = '';
+$postPassword = '';
 
-FLASH::addFlashes(array(
-    'It\'s a matter of time!',
-    'You got invalid email!',
-    'Fuck you boy!'
-), Index::__GetPage(), FLASH::ERROR, true);
-//FLASH::clearFlashes();
+
+
+if (DATA::__HasPostData())
+{
+    $type = DATA::__GetPOST('postType', true, true, false);
+    $postUsername = DATA::__GetPOST('postUsername', true, true, true);
+    $postPassword = DATA::__GetPOST('postPassword');
+    
+    FLASH::CheckAndAdd(array(
+        'Username should not contain spaces' => STR::__HasSpaces($postUsername),
+        'Username should contain letters and numbers only' => STR::__HasPunct($postUsername),
+        'Password cannot exceed 75 characters' => strlen($postPassword)>75
+    ), 'Success', Index::__GetPage(), Index::__GetPage(), true);
+    
+    if ( FLASH::__GetType()==FLASH::SUCCESS )
+    {
+        if ( $type=='LOGIN' )
+        {
+            $loginsucess = USER::Login($postUsername, $postPassword);
+            if ($loginsucess)
+            {
+                FLASH::AddFlash('Login success but redirection didn\'t work!', Index::__GetPage(), FLASH::SUCCESS, true);
+                UI::RedirectTo('user-home');
+            }
+            else
+            {
+                FLASH::AddFlash('Invalid username or password, please try again', Index::__GetPage(), FLASH::ERROR, true);
+            }
+        }
+        else if ( $type=='REGISTER' )
+        {
+            PARAMS::CreateMany(array(
+                [
+                    'name' => 'username',
+                    'value' => $postUsername
+                ], [
+                    'name' => 'password',
+                    'value' => $postPassword
+                ]
+            ), 'signup');
+            UI::RedirectTo('signup');
+        }
+    }
+}
 
 ?>
