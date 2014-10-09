6c656c73316a2d398ae70243581a3b49bd7ad461
Allen Linatoc <linatoc.allen@programmer.net>
2014-10-10 00:47:57 +0800

    Initial commit

---
+++ web+/aphp/js/anchorscroll.js
@@ -0,0 +1,11 @@
+/**
+ * This enables user to scroll to certain anchor in a page
+ */
+
+// This should work on elements
+
+function scrollTo(selector){
+    $('html, body').animate({
+            scrollTop: $(selector).offset().top
+    }, 'slow');
+}
